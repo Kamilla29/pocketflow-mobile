d0ebf79bdcb6c2c29ce560c16528b11e02d21851
Kamilla Kuanysheva <136432816+Kamilla29@users.noreply.github.com>
2026-09-05 15:07:07 +0200

    fix: type nested tab navigation

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,12 +1,14 @@
-export type RootStackParamList = {
-  Main: undefined;
-  PaymentSchedule: undefined;
-  ApplicationDetails: undefined;
-  Checklist: undefined;
-};
+import type { NavigatorScreenParams } from '@react-navigation/native';
 
 export type MainTabParamList = {
   Overview: undefined;
   Activity: undefined;
   Settings: undefined;
+};
+
+export type RootStackParamList = {
+  Main: NavigatorScreenParams<MainTabParamList> | undefined;
+  PaymentSchedule: undefined;
+  ApplicationDetails: undefined;
+  Checklist: undefined;
 };
